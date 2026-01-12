cae0681f6deac6b2f5da9a27cb938a73b802cf39
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-01-12 17:10:38 +0530

    Visa Card Added

--- lib/Screens/VisaCardScreen.dart
+++ lib/Screens/VisaCardScreen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:expensetracker/utils/Colors.dart';
+import 'package:expensetracker/utils/Texts.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class VisaCard extends StatelessWidget {
   const VisaCard({super.key});
@@ -15,7 +17,7 @@ class VisaCard extends StatelessWidget {
           decoration: BoxDecoration(
             // color: Colors.green,
             boxShadow: AppColors.shadows,
-            borderRadius: BorderRadius.circular(12)
+            borderRadius: BorderRadius.circular(12),
           ),
           margin: EdgeInsets.all(15),
           height: 250,
@@ -32,40 +34,62 @@ class content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       child: Stack(
         children: [
           Positioned(
             right: -150,
             child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.1),
-              shape: BoxShape.circle
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.1),
+                shape: BoxShape.circle,
+              ),
             ),
-          )),
+          ),
           Positioned(
             left: -200,
             bottom: -470,
             child: Container(
-            height: 600,
-            width: 700,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.1),
-              shape: BoxShape.circle
+              height: 600,
+              width: 700,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.1),
+                shape: BoxShape.circle,
+              ),
             ),
-          )),
+          ),
           Positioned(
-            top: 20,
-            left: 15 ,
-            child: Container(
-              height: 30,
-              child: Image.asset("assets/logo.png"),
+            top: 25,
+            left: 15,
+            child: Container(height: 30, child: Image.asset("assets/logo.png")),
+          ),
+          Positioned(
+            top: 62,
+            left: 25,
+            child: ModifiedText(
+              text: "it's where you want to be",
+              color: Colors.grey.shade900,
+              size: 15,
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 15,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("6532 4567 3414 7683",style: GoogleFonts.sourceCodePro(fontSize: 24,color: Colors.grey.shade700,fontWeight: FontWeight.bold),),
+                Text("SHAURYA THAKUR",style: GoogleFonts.sourceCodePro(fontSize: 20,color: Colors.grey.shade600,fontWeight: FontWeight.bold),),
+              ],
             )
-            ),
+          ),
           Positioned(
-            child: )
+            right: 10,
+            child: Container( 
+            height: 80,
+            child: Image.asset("assets/chip.png")))
         ],
       ),
     );
--- lib/utils/Texts.dart
+++ lib/utils/Texts.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class modifiedText extends StatelessWidget {
+class ModifiedText extends StatelessWidget {
   final String text;
   final Color color;
   final double size;
-  const modifiedText({super.key, required this.text, required this.color, required this.size});
+  final FontWeight fontWeight;
+
+  const ModifiedText({
+    super.key,
+    required this.text,
+    required this.color,
+    required this.size,
+    this.fontWeight = FontWeight.normal,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: GoogleFon,
+      style: GoogleFonts.mada(
+        color: color,
+        fontSize: size,
+        fontStyle: FontStyle.italic,
+        fontWeight: fontWeight,
+      ),
     );
   }
 }
